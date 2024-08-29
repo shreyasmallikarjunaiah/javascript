0a87988b8b2bdc49f366397c9ea21c63613904e0
shreyasshreyi123@gmail.com <shreyasshreyi123@gmail.come>
2024-08-29 09:28:28 +0530

    first commit

---
+++ (1)   DATATYPES/DataTypes.js
@@ -0,0 +1,34 @@
+//7 DATA TYPE
+/*  1.NUMBER
+	2.STRING
+	3.BOOLEAN
+	4.UNDEFINED
+	5.NULL
+	6.SYMBOL
+	7.BIGLINT*/
+//IN JAVASCRIPT WE CAN IGNOR THE DATA TYPE BECOUSE IT IS DYNAMICALY TYPED LANGUAGE
+
+
+let number=27
+let name='shreyas'
+let find= true
+
+console.log(number)
+console.log(name)
+console.log(find)
+
+// WE CAN FIND THE DATA TYPE BY "typeof" in cosole
+
+console.log(typeof number)
+console.log(typeof name)
+console.log(typeof find)
+
+//WE CAN UPDATE THE VALUES OF THE VARIALBES BY  
+
+number=55
+name='shreyi'
+find=false
+
+console.log(number)
+console.log(name)
+console.log(find)
