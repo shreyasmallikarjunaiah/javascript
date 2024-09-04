7efde2c93447ffcdb7bf494d35731fba336cc289
[shreyas malliarjunaiah] <[shreyasshreyi123@gmail.com]>
2024-09-04 09:10:55 +0530

    commit

--- PRECEDENCE_OPARATOR.js
+++
@@ -1,3 +0,0 @@
-/*WE CAN CHECK THE PRECEDENCE USING BELOW LINK
-https://developer.mozilla.org/en-US/docs/Web/
-JavaScript/Reference/Operators/Operator_precedence*/
--- coercion.js
+++
@@ -1,6 +0,0 @@
-console.log('I am '+23+' year old')//I am 23 year old 	
-
-console.log('33'+22+'11')//332211
-
-console.log('33'-22-'11')//0
-
