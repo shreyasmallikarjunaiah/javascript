84bcb2901760fa34e6beb6c28209c8165e4ec546
SHREYAS O M <shreyasshreyi123@gmail.com>
2024-09-12 08:36:54 +0530

    Add files via upload

---
+++ ternary.js
@@ -0,0 +1,8 @@
+const age=23;
+age >=18 ? console.log('i want get marry'):console.log('i dont want marrage');
+
+
+// USING TERNARY OPARATORS//
+
+const number=5;
+console.log (`i like to drink ${number >= 18 ? 'water' : 'wine'}`)
