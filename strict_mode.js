faa4fed51e7d65de0bfe59e2024c1b4df010bda6
SHREYAS O M <shreyasshreyi123@gmail.com>
2024-09-13 10:09:42 +0530

    Add files via upload

---
+++ strict_mode.js
@@ -0,0 +1,9 @@
+'use strict';
+let hasDrivingLicense=true;
+const passTest=true;
+
+if (passTest)hasDrivingicense=true // 'L' IS MISSING//
+if (hasDrivingLicense)
+	console.log('you can drive')
+
+// IF WE NOT USE STRICT MODE IT WILL GIVE OUT PUT//
